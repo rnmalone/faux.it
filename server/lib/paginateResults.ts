c65b1c5fe6ed7ce5a724ec9125892bd868ca655e
Rory malone <rory.malone@hotmail.com>
2020-09-12 18:17:40 +0100

    feat: add employee list pagination and employee query

---
+++ server/lib/paginateResults.ts
@@ -0,0 +1,5 @@
+import {IPagingInput} from "../@types/Paging";
+
+export default function paginateResults(items: any[], pagingInput: IPagingInput) {
+    return items.slice(pagingInput.offset, pagingInput.offset + pagingInput.limit)
+}
--- server/resolvers/employee.ts
+++ server/resolvers/employee.ts
@@ -1,9 +1,45 @@
 import Employee from "../entities/Employee";
+import {IContext} from "../server";
+import {paginateResults} from "../lib";
+import {Sale} from "../entities";
 
 const employeeResolver = {
     Query: {
-        employeeList: async(root: any, { paging }: any) => {
+        employeeList: async(root: any, { paging }: any, { connection }: IContext) => {
+            console.log('root', root)
+            const items: Employee[] = await connection.manager.find(Employee)
 
+            const paginatedItems = paginateResults(items, paging)
+
+            return {
+                count: items.length,
+                items: paginatedItems,
+                // @ts-ignore
+                facets: []
+            }
+        },
+        employee: async(root: any, { id, }: any, { connection }: IContext) => {
+            const employee = await connection
+                .getRepository(Employee)
+                .createQueryBuilder("employee")
+                .where('Employee.id = :id', { id })
+                .getOne()
+
+            if(!employee) {
+                //TODO error handle
+
+                return {}
+            }
+
+            const sales = await connection
+                .getRepository(Sale)
+                .createQueryBuilder('sale')
+                .where('Sale.employeeId = :id', { id })
+                .getMany();
+
+            employee.sales = sales
+
+            return employee
         }
     }
 }
