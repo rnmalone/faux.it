e041a5c31bd98adc35f9508b1a2ea725ad0a8b04
Rory malone <rory.malone@hotmail.com>
2020-09-13 20:29:59 +0100

    feat: add test data

--- server/data/sales.ts
+++ server/data/sales.ts
@@ -99,7 +99,139 @@ const sales: SaleDTO[] = [
         status: SaleStatus.Complete,
         dateOpened: new Date(),
         dateClosed: new Date()
-    }
+    },
+    {
+        id: 10,
+        employeeId: 2,
+        item: 'Boaty McBoatface',
+        agreedPrice: 170000000,
+        ticketPrice: 170000000,
+        customerName: 'Atlantic Research',
+        status: SaleStatus.Complete,
+        dateOpened: new Date(),
+        dateClosed: new Date()
+    },
+    {
+        id: 11,
+        employeeId: 2,
+        item: 'Water Bird',
+        agreedPrice: 120000,
+        ticketPrice: 123000,
+        customerName: 'Mikkel Spikken',
+        status: SaleStatus.AwaitingPayment,
+        dateOpened: new Date(),
+        dateClosed: null
+    },
+    {
+        id: 12,
+        employeeId: 2,
+        item: 'Grand Mister',
+        agreedPrice: 450000,
+        ticketPrice: 450000,
+        customerName: 'Lucinda Jo',
+        status: SaleStatus.AwaitingPayment,
+        dateOpened: new Date(),
+        dateClosed: null
+    },
+    {
+        id: 13,
+        employeeId: 2,
+        item: 'The Old Ragdoll',
+        agreedPrice: 278000,
+        ticketPrice: 285000,
+        customerName: 'Rosie and Jim',
+        status: SaleStatus.Complete,
+        dateOpened: new Date(),
+        dateClosed: new Date()
+    },
+    {
+        id: 14,
+        employeeId: 3,
+        item: 'F-17',
+        agreedPrice: 37000000,
+        ticketPrice: 37000000,
+        customerName: 'Mr McTrustworthy',
+        status: SaleStatus.Closed,
+        dateOpened: new Date(),
+        dateClosed: new Date()
+    },
+    {
+        id: 15,
+        employeeId: 3,
+        item: 'Millenium Falcon',
+        agreedPrice: 10000,
+        ticketPrice: 10000,
+        customerName: 'Han Solo',
+        status: SaleStatus.Complete,
+        dateOpened: new Date(),
+        dateClosed: new Date()
+    },
+    {
+        id: 16,
+        employeeId: 3,
+        item: 'Concorde',
+        agreedPrice: 100000,
+        ticketPrice: 100000,
+        customerName: 'Air France',
+        status: SaleStatus.Complete,
+        dateOpened: new Date(),
+        dateClosed: new Date()
+    },
+    {
+        id: 17,
+        employeeId: 3,
+        item: 'Concorde',
+        agreedPrice: 1000000,
+        ticketPrice: 1000000,
+        customerName: 'Air France',
+        status: SaleStatus.Complete,
+        dateOpened: new Date(),
+        dateClosed: new Date()
+    },
+    {
+        id: 18,
+        employeeId: 4,
+        item: 'Dalorean DMC',
+        agreedPrice: 100000,
+        ticketPrice: 100000,
+        customerName: 'Weird bloke',
+        status: SaleStatus.Complete,
+        dateOpened: new Date(),
+        dateClosed: new Date()
+    },
+    {
+        id: 19,
+        employeeId: 4,
+        item: 'Vauxhaul Carlton',
+        agreedPrice: 958000,
+        ticketPrice: 100000,
+        customerName: 'David McDavey',
+        status: SaleStatus.Complete,
+        dateOpened: new Date(),
+        dateClosed: new Date()
+    },
+    {
+        id: 20,
+        employeeId: 4,
+        item: 'Vauxhaul Corsa',
+        agreedPrice: 950,
+        ticketPrice: 1050,
+        customerName: 'Ste Jones',
+        status: SaleStatus.Complete,
+        dateOpened: new Date(),
+        dateClosed: new Date()
+    },
+    {
+        id: 21,
+        employeeId: 4,
+        item: 'Ferarri 458 Italia',
+        agreedPrice: 212000,
+        ticketPrice: 220000,
+        customerName: 'James MAy',
+        status: SaleStatus.Complete,
+        dateOpened: new Date(),
+        dateClosed: new Date()
+    },
 ]
 
 export default sales;
