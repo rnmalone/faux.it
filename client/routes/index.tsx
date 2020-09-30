233659b65ae9f501c376d5d04570f0ebfdeefd67
unknown <rory.malone@hotmail.com>
2020-10-01 00:36:51 +0100

    feat: add transition on routes

--- client/routes/index.tsx
+++ client/routes/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import {Route, Switch} from "react-router";
+import {Route, Switch, useLocation} from "react-router";
 import SaleList from "./SaleList";
 import EmployeeList from "./EmployeeList";
 import Sale from "./Sale";
 import Employee from "./Employee";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 
 export default () => {
+    const location = useLocation();
     return (
-        <Switch>
-            <Route exact path="/employees" component={EmployeeList}/>
-            <Route path="/sales" coponent={SaleList}/>
-            <Route path="/sale/:sale" component={Sale}/>
-            <Route path="/employee/:id" component={Employee}/>
-        </Switch>
+        <TransitionGroup>
+            <CSSTransition
+                key={location.key}
+                classNames="opacity"
+                timeout={1000}
+            >
+                <Switch>
+                    <Route exact path="/employees" component={EmployeeList}/>
+                    <Route path="/sales" coponent={SaleList}/>
+                    <Route path="/sale/:sale" component={Sale}/>
+                    <Route path="/employee/:id" component={Employee}/>
+                </Switch>
+            </CSSTransition>
+        </TransitionGroup>
     )
 }
